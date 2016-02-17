f5ca523f90b67af1d6cf0ace620f8dbc048cfadd
Patrick Seelinger <Patrick Seelinger>
2016-02-17 17:27:38 -0500

    got sub-tabs working and cleaned up the json file

--- js/app.js
+++ js/app.js
@@ -69,12 +69,12 @@ app.controller('BaseController', ['$http', function($http) {
     this.candidates = [];
     this.isClicked = false;
     this.raceHtml = "";
-    this.candInfo = "<div class='tab-content'>";
+    this.candInfo = "";
     this.raceCandidates = [];
     this.currentState = "";
     //showRaceClick: displays info about each senate race when the user clicks on the appropriate state
     this.showRaceClick = function(stateClicked){
-      this.raceHtml = "<div class='col-md-6'><h1>" + stateClicked + "</h1><ul class='nav nav-pills nav-stacked' id='cand-info-tabs'>";
+      this.raceHtml = "<h1>" + stateClicked + "</h1><ul class='nav nav-pills nav-stacked' id='cand-info-tabs'>";
       this.raceCandidates = [];
       if (this.currentState == stateClicked){
         this.isClicked = !this.isClicked;
@@ -87,17 +87,19 @@ app.controller('BaseController', ['$http', function($http) {
           //add senators to an array so we can copnstruct individual divs for them
           this.raceCandidates.push(this.senators[j].name);
           //construct the divs
-          this.raceHtml += "<li><a data-toggle='pill' href='#" + this.senators[j].last + "'>" + this.senators[j].name + "</a></li>";
+          this.raceHtml += "<li><a data-toggle='pill' href='#" + this.senators[j].last + "'>" + this.senators[j].name  + " (" + this.senators[j].party + ")" + "</a></li>";
         }
       };
-      this.raceHtml += "</ul></div>";
+      this.raceHtml += "</ul>";
       this.raceCandidates = [];
       for(var j in this.senators){
         if(this.senators[j].state === stateClicked ){
           //add senators to an array so we can copnstruct individual divs for them
           this.raceCandidates.push(this.senators[j].name);
           //construct the divs
-          this.candInfo += "<div id='" + this.senators[j].last + "' class='tab-pane'><h3>" + this.senators[j].name + "</h3></div>";
+          this.candInfo += "<div id='" + this.senators[j].last + "' class='tab-pane'><h3>" + this.senators[j].name ;
+          this.candInfo += " (" + this.senators[j].party + ")";
+          this.candInfo += "</h3></div>";
         }
       };
       //write the divs to the page
